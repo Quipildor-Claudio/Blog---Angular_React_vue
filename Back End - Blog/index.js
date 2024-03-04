c1e1b40b9f588d7573eb0049330c5af1aea753ac
Claudio Quipildor <claudioq00@gmail.com>
2024-03-03 21:52:10 -0300

    add cords

--- Back End - Blog/index.js
+++ Back End - Blog/index.js
@@ -7,7 +7,14 @@ var port =dbURL.port;
 app.use(express.json()); // 
 
 mongoose.connect(dbURL.dbURL);
-
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use('/api',article_routes);
 
