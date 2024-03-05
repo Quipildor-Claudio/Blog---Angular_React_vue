9e3c209f715564d3913f9732ca7e2abdcf9d4141
Claudio Quipildor <claudioq00@gmail.com>
2024-03-04 23:15:38 -0300

    adapt maquetacion to components  angular

--- blog-app-angular/src/app/app.component.ts
+++ blog-app-angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,SidebarComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,SidebarComponent,SliderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
